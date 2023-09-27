b34d8e679dac8d637f8060774f044f495c0eaa2e
SuronjoyRoy <suronjoyroy21@gmail.com>
2023-09-27 12:38:55 +0600

    full responsive donation website

--- src/Components/Header/Banner/Banner.jsx
+++ src/Components/Header/Banner/Banner.jsx
@@ -13,9 +13,9 @@ const Banner = () => {
             </div>
         <div>
         <div className="form-control pt-10 ">
-                <p className="absolute top-[70%] lg:top-[38%] left-[10%] lg:left-[30%] text-3xl font-bold text-[#0B0B0B] lg:mb-15" >I Grow By Helping People In Need</p>
+                <p className="absolute top-[70%] lg:top-[38%] left-[10%] md:left-[18%] lg:left-[30%] text-3xl font-bold text-[#0B0B0B] lg:mb-15" >I Grow By Helping People In Need</p>
                 
-            <div className="input-group w-48 lg:w-72 absolute top-[50%] lg:top-[47%] left-[3%] lg:left-[35%]">
+            <div className="input-group w-48 lg:w-72 absolute top-[50%] lg:top-[47%] left-[3%] md:left-[30%] lg:left-[35%]">
                     <input type="text" placeholder="Search…" className="input input-bordered" />
                     <button className="btn btn-secondary  bg-[#FF444A]">
                     Search
